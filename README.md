bb1ce62774d60a63a54188504c72287b3bc3b9e5
Wahyu Kristianto <w.kristories@gmail.com>
2014-01-18 20:11:48 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,108 @@
+# Templator
+
+A Clean Solution for Templating with [Twig](twig.sensiolabs.org)
+
+## Installation
+
+Add the following into your `composer.json` file:
+
+    {
+        "require": {
+            "kristories/templator": "*"
+        }
+    }
+
+Then run
+
+    composer install
+
+
+## Usage
+
+### Basic
+
+    $templator = new \Templator\Templator();
+
+    // Set data
+    $templator->data('foo', 'bar');
+
+    // Render
+    // Let's define a base template /templates/base/base_template.html
+    // and a child template         /templates/pages/page.html
+    echo $templator->render('base_template', 'page');
+
+### Config
+
+    $templator = new \Templator\Templator([
+        'path'  => [
+            'root'      => 'templates',
+            'base'      => 'base',
+            'pages'     => 'pages',
+            'widgets'   => 'widgets'
+        ],
+        'cache' => NULL
+    ]);
+
+Structure :
+
+    ├── templates
+    |   ├── base
+    |   └── pages
+    |   └── widgets
+    └── cache
+
+### Data
+
+    $templator->data('foo', 'bar');
+    // or
+    $templator->data([
+        'foo' => 'bar',
+        'bar' => 'baz'
+    ]);
+
+### Widgets
+
+    $templator->widgets([
+        'header' => ['logo', 'mainmenu', 'search'],
+        'footer' => ['copyright, 'footermenu']
+    ]);
+
+### Example
+
+`/index.html`
+
+    $templator = new \Templator\Templator();
+
+    $data = [
+        'hello' => 'world'
+    ];
+    $widgets = [
+        'header' => ['logo']
+    ];
+
+    // Support method chaining
+    $templator->data($data)->widgets($widgets);
+
+    // Output
+    echo $templator->render('base_template', 'page');
+
+`/templates/base_template.html`
+    
+    {% block header %}{% endblock %}
+    {% block content %}{% endblock %}
+
+`/templates/pages/page.html`
+    
+    <h3>Hello {{ hello }}!</h3>
+
+`/templates/widgets/logo.html`
+
+    <h1>LOGO</h1>
+
+## Why Twig?
+
+### Twig is a modern template engine for PHP
+
+- **Fast**: Twig compiles templates down to plain optimized PHP code. The overhead compared to regular PHP code was reduced to the very minimum.
+- **Secure**: Twig has a sandbox mode to evaluate untrusted template code. This allows Twig to be used as a template language for applications where users may modify the template design.
+- **Flexible**: Twig is powered by a flexible lexer and parser. This allows the developer to define its own custom tags and filters, and create its own DSL.
---
+++ src/Templator/Templator.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Templator;
+
+class Templator {
+
+    /**
+     * Templator version
+     * @var string
+     */
+    public $version = '0.0.1';
+
+    /**
+     * Configuration
+     * @var array
+     */
+    protected $_config = array(
+        'path'  => array(
+            'root'      => 'templates',
+            'base'      => 'base',
+            'pages'     => 'pages',
+            'widgets'   => 'widgets'
+        ),
+        'cache' => NULL
+    );
+
+    /**
+     * Data
+     * @var array
+     */
+    protected $_data    = array();
+
+    /**
+     * Default base template
+     * @var string
+     */
+    protected $_base    = 'default';
+
+    /**
+     * Widgets
+     * @var array
+     */
+    protected $_widgets = array();
+
+
+    /**
+     * Constructor
+     * 
+     * @param array $config
+     */
+    public function __construct($config = array())
+    {
+        if(is_array($config))
+        {
+            $this->_config = array_merge($this->_config, $config);
+        }
+    }
+
+    /****************************************************************************/
+
+    /**
+    * Set data
+    * 
+    * @param  array|string   $key  
+    * @param  string         $value
+    * @return \Templator\Templator
+    */
+    public function data($key = NULL, $value = NULL)
+    {
+        if($key)
+        {
+            if(is_array($key))
+            {
+                foreach ($key as $k => $v)
+                {
+                    $this->_data[$k] = $v;
+                }
+            }
+            else
+            {
+                $this->_data[$key] = $value;
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set widgets
+     * 
+     * @param  array  $widgets
+     * @return \Templator\Templator
+     */
+    public function widgets($widgets = array())
+    {
+        // Check
+        if($widgets AND is_array($widgets))
+        {
+            $this->_widgets = $widgets;
+
+            return $this;
+        }
+
+        die('Widgets must be an array');
+    }
+
+    /**
+     * Renders a template
+     * 
+     * @param  string $base
+     * @param  string $page
+     * @return string The rendered template
+     */
+    public function render($base = NULL, $page = NULL)
+    {
+        // Checking
+        ($base AND $page) OR die('Base template and page must be set!');
+
+        // Base template
+        $file  = "{% extends '/" . $this->_config['path']['base'] . '/' . $base . ".html' %}";
+
+        // Widgets
+        if($this->_widgets)
+        {
+            foreach ($this->_widgets as $position => $items)
+            {
+                $file .= "{% block " . $position . " %}";
+                
+                foreach ($items as $item)
+                {
+                    $file .= "{% include '" . $this->_config['path']['widgets'] . '/' . $item . ".html' %}";
+                }
+
+                $file .= "{% endblock %}";
+            }
+        }
+
+        // Content
+        $file .= "{% block content %}";
+        $file .= file_get_contents($this->_config['path']['root'] . '/' . $this->_config['path']['pages'] . '/' . $page . '.html');
+        $file .= "{% endblock %}";
+
+        // Twig it!
+        $config             = array('cache' => $this->_config['cache']);
+        $loader_filesystem  = new \Twig_Loader_Filesystem($this->_config['path']['root']);
+        $loader_string      = new \Twig_Loader_String();
+        $loader             = new \Twig_Loader_Chain(array($loader_filesystem, $loader_string));
+        $twig               = new \Twig_Environment($loader, $config);
+
+        // Return
+        return $twig->render($file, $this->_data);
+    }
+
+}
